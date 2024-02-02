793ef90fccccb1a0de7f314836818e90417ad6bb
Lalit Kumar <lalit007lodhi@gmail.com>
2024-02-02 18:52:34 +0530

    second commit

--- backend/Controllers/fetchControllers.js
+++ backend/Controllers/fetchControllers.js
@@ -3,15 +3,18 @@ const Story =require('../models/dataSchema')
 
 
 
-const fetchData=async (req, res) => {
-    try {
-        const stories = await Story.find();
-        res.json(stories);
-        console.log(stories);
-      } catch (error) {
-        console.error('Error fetching stories:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
+const fetchData = async (req, res) => {
+  try {
+      // Fetch stories sorted by time field in ascending order
+      const stories = await Story.find().sort({ time: -1 });
+
+      // Respond with the sorted data
+      res.json(stories);
+      console.log(stories);
+  } catch (error) {
+      console.error('Error fetching stories:', error);
+      res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 
