c14d1338877a83ccc904ac3bb6a804259862e695
Lalit Kumar <lalit007lodhi@gmail.com>
2024-02-12 00:05:48 +0530

    done code

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb://localhost:27017'; 
+const dbURI = 'mongodb+srv://wizardlalit007:fxcAUUmj4dS748bv@cluster0.m5vd6qo.mongodb.net/?retryWrites=true&w=majority'; 
 
 mongoose.connect(dbURI)
   .then(() => {
